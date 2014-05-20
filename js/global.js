6ca02beb2a8669d546f47fb7cd4d9ccfc482de25
Chaos Xu (zn徐远翔) <15887805082@qq.com>
2014-05-20 10:36:08 +0800

    1

--- js/global.js
+++ js/global.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-	$('body').hide();
 	$('.subnav').slick({
 		slidesToShow : 4,
 		slidesToScroll : 4,
 		slide : 'span'
 	});
-	$('body').fadeIn(1000);
+	$('#loading').hide();
 });
